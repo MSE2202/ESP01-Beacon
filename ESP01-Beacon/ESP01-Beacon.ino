a3209497f4114a4367a484a7d07aea4e8340eb0f
naish-uwo <63020577+naish-uwo@users.noreply.github.com>
2023-12-20 09:40:02 -0500

    Further cleanup

--- ESP01-Beacon/ESP01-Beacon.ino
+++ ESP01-Beacon/ESP01-Beacon.ino
@@ -1,125 +1,127 @@
-//MSE 2202 esp01s Beacon code
-
-//EJ Porter Dec 2023 Ver 1
-
-
-//Modified code from:
-// High-accuracy square wave generator
-// by James Swineson <github@public.swineson.me>, 2017-10
-// https://gist.github.com/Jamesits/92394675c0fe786467b26f90e95d3904
-// See https://blog.swineson.me/implementation-of-6mbps-high-speed-io-on-esp8266-arduino-ide/
-
+// 
+// MSE 2202 ESP-01S Beacon Code
+// 
+//  Language: Arduino (C++)
+//  Target:   ESP-01S
+//  Authors:  Eugen Porter and Michael Naish
+//  Date:     2023 12 19 
+//
+//  Modified code from:
+//    High-accuracy square wave generator
+//    by James Swineson <github@public.swineson.me>, 2017-10
+//    https://gist.github.com/Jamesits/92394675c0fe786467b26f90e95d3904
+//    See https://blog.swineson.me/implementation-of-6mbps-high-speed-io-on-esp8266-arduino-ide/
+//
 
 #include <Esp.h>
 #include <esp8266_peri.h>
 #include <eagle_soc.h>
 #include <uart_register.h>
-// #include <ESP8266WiFi.h>
-
-#define TX_DATA 0x55
 
 #define WDT_CTL 0x60000900
 #define WDT_CTL_ENABLE (BIT(0))
+
+// compare 2 unsigned values
+// true if X > Y while for all possible (X, Y), X - Y < Z
+#define TIME_CMP(X, Y, Z) (((X) - (Y)) < (Z))
 
 // Configurable from 0 to 15; 16 not available
 // Please refer to your breakout board datasheet for pin mapping
 // WARNING: some pins are used internally for connecting the ESP to ROM chip;
 // DO NOT USE THEM or your ESP will be bricked
-#define PINOUT 0  //38 kHz out
+const int cCarrierPin = 0;                         // GPIO for 38 kHz carrier
 
-#define NUM_CHARS 5
+const int cSerialPin = 2;                          // GPIO for TX1
+const char cTXData = 0x55;                         // U
+const int cNumBytes = 1;                           // number of bytes to transmit in a burst
+const int cBurstSpacing = 5000;                    // time between bursts
 
-double freq;    // Hz
-double offset;  // percent (0.0 to 1.0)
-double width;   // percent (0.0 to 1.0)
+double freq;                                       // carrier frequency in Hz
+double offset;                                     // percent (0.0 to 1.0)
+double width;                                      // percent (0.0 to 1.0)
+
+unsigned int TXDelay;                              //
 
 // unit: microsecond
-unsigned long cycle_time;
-unsigned long raising_edge;
-unsigned long falling_edge;
-unsigned long prev_micros;
+unsigned long cycleTime;                           // carrier cycle time in microseconds
+unsigned long risingEdge;                          // 
+unsigned long fallingEdge;
+unsigned long prevMicros;
 
-// compare 2 unsigned value
-// true if X > Y while for all possible (X, Y), X - Y < Z
-#define TIME_CMP(X, Y, Z) (((X) - (Y)) < (Z))
+ADC_MODE(ADC_VCC);                                 // set up ADC to read VCC pin
 
 inline void setHigh() {
-  GPOS = (1 << PINOUT);
+  GPOS = (1 << cCarrierPin);
 }
 
 inline void setLow() {
-  GPOC = (1 << PINOUT);
+  GPOC = (1 << cCarrierPin);
 }
-
-unsigned int ui_Tx_Delay;
-
-ADC_MODE(ADC_VCC);  // set up adc to read vcc pin
 
 void setup() {
   // disable hardware watchdog
   CLEAR_PERI_REG_MASK(WDT_CTL, WDT_CTL_ENABLE);
   // disable software watchdog
   ESP.wdtDisable();
-  // set IO pin mode
-  pinMode(PINOUT, OUTPUT);
+  // set IO pin mode for carrier signal
+  pinMode(cCarrierPin, OUTPUT);
 
   //set up  Serial1
   //UART_FIFO(1) = {0xff, 0xFF, 0xFF, 0x55, 0xFF, 0xFF};
   // Set up UART1 pins
   // GPIO2 is TX, GPIO3 is RX
-  pinMode(2, FUNCTION_2);  // Configure for TX1
+  pinMode(cSerialPin, FUNCTION_2);  // Configure for TX1
   //pinMode(3, FUNCTION_2); // RX
 
   // Configure UART1 with desired parameters
   // For example, 2400 baud rate, 8 data bits, no parity, 1 stop bit
-  Serial1.begin(2400, SERIAL_8N2);
+  Serial1.begin(1200, SERIAL_8N1);
 
   // calculate arguments
-  freq = 38000;
+  freq = 38000;  
   width = 0.5;
   offset = 0.0;
 
-  cycle_time = 1000000 / freq;
-  raising_edge = (unsigned long)(offset * cycle_time) % cycle_time;
-  falling_edge = (unsigned long)((offset + width) * cycle_time) % cycle_time;
+  cycleTime = 1000000 / freq;
+  risingEdge = (unsigned long)(offset * cycleTime) % cycleTime;
+  fallingEdge = (unsigned long)((offset + width) * cycleTime) % cycleTime;
 
-  prev_micros = micros();
+  prevMicros = micros() + cycleTime;
 }
 
 void loop() {
-// do pinout shifting
+// do pinout shifting for carrier
   if (width + offset < 1) {
-    // raising edge should appear earlier
-    while (TIME_CMP(micros(), prev_micros + raising_edge, cycle_time))
-      ;
-    setHigh();
-    while (TIME_CMP(micros(), prev_micros + falling_edge, cycle_time))
-      ;
-    setLow();
+    // rising edge should appear earlier
+    while (TIME_CMP(micros(), prevMicros + risingEdge, cycleTime)) {
+      setHigh();
+    }
+    while (TIME_CMP(micros(), prevMicros + fallingEdge, cycleTime)) {
+      setLow();
+    }
   } 
   else {
     // falling edge should appear earlier
-    while (TIME_CMP(micros(), prev_micros + falling_edge, cycle_time))
-      ;
-    setLow();
-    while (TIME_CMP(micros(), prev_micros + raising_edge, cycle_time))
-      ;
-    setHigh();
+    while (TIME_CMP(micros(), prevMicros + fallingEdge, cycleTime)) {
+      setLow();
+    }
+    while (TIME_CMP(micros(), prevMicros + risingEdge, cycleTime)) {
+      setHigh();
+    }
   }
-  prev_micros += cycle_time;
+  prevMicros += cycleTime;
   // Check if the UART1 TX buffer is empty
   if (!(READ_PERI_REG(UART_STATUS(UART1)) & (UART_TXFIFO_CNT << UART_TXFIFO_CNT_S))) {
     // Send the byte if the buffer is empty
     // Fill the UART1 TX FIFO with the provided data
-    ui_Tx_Delay++;
-    if (ui_Tx_Delay > 5000)  //delay between sent bytes
-    {
-      for (int i = 0; i < NUM_CHARS; i++) {
-        WRITE_PERI_REG(UART_FIFO(UART1), TX_DATA);  //Tx_data[TX_Buffer_index]);
+    TXDelay++;
+    if (TXDelay > cBurstSpacing) {                 // delay between sent bursts
+      for (int i = 0; i < cNumBytes; i++) { 
+        WRITE_PERI_REG(UART_FIFO(UART1), cTXData); //Tx_data[TX_Buffer_index]);
       }
-      ui_Tx_Delay = 0;
-      if (ESP.getVcc() <= 255) {
-        ESP.deepSleep(0);  //goes to sleep because battery is low. unplug and recharge
+      TXDelay = 0;                                 // reset TXDelay
+      if (ESP.getVcc() <= 255) {                   // if battery voltage is low
+        ESP.deepSleep(0);                          // put device to sleep -> unplug and recharge
       }
     }
   }
